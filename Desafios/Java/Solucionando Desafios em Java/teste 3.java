9b825cde485da4f98ca0afb4a22ac7ed7e616e1b
Leonardo Oliveira <62403037+srgoogle23@users.noreply.github.com>
2021-06-02 21:19:59 -0300

    updates

--- Desafios/Java/Solucionando Desafios em Java/teste 3.java
+++ Desafios/Java/Solucionando Desafios em Java/teste 3.java
@@ -1,3 +1,20 @@
+/*
+ 
+ QUESTÃO
+ Neste problema sua tarefa será ler vários números e em seguida dizer quantas 
+ vezes cada número aparece na entrada de dados, ou seja, deve-se escrever cada 
+ um dos valores distintos que aparecem na entrada por ordem crescente de valor.
+ ENTRADA
+ A entrada contém apenas 1 caso de teste. A primeira linha de entrada contem 
+ um único inteiro N, que indica a quantidade de valores que serão lidos para 
+ X (1 <= X <= 2000) logo em seguida. Com certeza cada número não aparecerá mais 
+ do que 20 vezes na entrada de dados.
+ 
+ SAÍDA
+ Imprima a saída de acordo com o exemplo fornecido abaixo, indicando quantas vezes 
+ cada um deles aparece na entrada por ordem crescente de valor.
+*/
+
 import java.io.IOException;
 import java.util.Map.Entry;
 import java.util.Scanner;
